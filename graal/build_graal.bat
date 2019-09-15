d2a7b499686670268c5bfdcd18bd8293779e2742
jeag2002 <jeag2002@gmail.com>
2019-09-15 11:20:41 +0200

    micronaut -3

--- graal/build_graal.bat
+++ graal/build_graal.bat
@@ -1,7 +1,7 @@
 @echo off
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64 /Debug
 call gradlew assemble
-%GRAAL_HOME%\bin\native-image --class-path ./build/libs/graal-*-all.jar  -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" -H:Name=graal -H:Class=graal.Application -H:+ReportUnsupportedElementsAtRuntime -H:+AllowVMInspection 
+%GRAAL_HOME%\bin\native-image --class-path ./build/libs/graal-*-all.jar  -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" -H:Name=graal -H:Class=graal.Application -H:+ReportUnsupportedElementsAtRuntime -H:+AllowVMInspection -H:+ReportExceptionStackTraces --initialize-at-run-time=io.netty.handler.ssl.JdkSslContext
 
 REM -H:EnableURLProtocols=http
 REM -H:+JNI -H:ReflectionConfigurationFiles="./netty_reflection_config.json" --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder
